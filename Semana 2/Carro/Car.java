5a8aa091bba99b88e77da189dd6801c1b758657b
Moises <moisesdecarvalho2005@gmail.com>
2026-08-28 16:55:24 -0300

    Add files via upload

---
+++ Semana 2/Carro/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    int Ano;
+    String Marca;
+    String Cor;
+
+    Car(int ano, String marca, String cor){
+
+        this.Ano = ano;
+        this.Marca = marca;
+        this.Cor = cor;
+    }
+
+}
---
+++ Semana 2/Pessoa/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    int Idade;
+    String Nome;
+    Double Altura;
+
+    Person(int idade, String nome, Double altura){
+        
+        this.Altura = altura;
+        this.Idade = idade;
+        this.Nome = nome;
+    }
+
+}
